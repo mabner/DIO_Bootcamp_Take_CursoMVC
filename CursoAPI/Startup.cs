eb69000c4cc0fa56a3ca036b7df0bc7a9c744121
Marcos Leite <m.abner@live.co.uk>
2021-12-13 00:49:33 -0300

    adds swagger config

--- CursoAPI/Startup.cs
+++ CursoAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace CursoAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Curso API", Version = "v1" }); });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,11 @@ namespace CursoAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            // Defines the endpoint for swagger
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint( "/swagger/v1/swagger.json", "CursoAPI" ); });
 
             app.UseHttpsRedirection();
 
